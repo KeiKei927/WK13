c1bd731e03f80a1e5039ccbc7a48a668167faa2c
That Guy <thatguy@Huawei-Matebook-3.local>
2025-04-22 18:49:35 -0400

    level_two.py

---
+++ level_two.py
@@ -0,0 +1,4 @@
+mix_of_stuff = ["7", "hi", "3.14", "true"]
+playlist_for_coding = ["The Book Of Love by Slum Village", "Sienna by The Marias", "Rainy Tapestry by Lamp", "Ottolenghi by Loyle Carner", "Hard to Let Go by Men I Trust"]
+
+playlist_for_coding.append("Never Gonna Give You Up by Rick Astley cant imagine getting rickrolled in 2025")
